f49c45944d99f58560a06b13ccf295d9c457a669
Tonton Jo <60965766+Tontonjo@users.noreply.github.com>
2022-04-02 01:43:31 +0200

    Update HDRtoSDR_converter.sh

--- HDRtoSDR_converter.sh
+++ HDRtoSDR_converter.sh
@@ -59,7 +59,7 @@ for C in $COLORS; do
 done    
 if [ "${COLORSPACE}" = "bt2020nc" ] && [ "${COLORTRANSFER}" = "smpte2084" ] && [ "${COLORPRIMARIES}" = "bt2020" ]; then 
         echo "- Found HDR file $mkv"
-		echo "- Found HDR file $mkv" >> $outputpath/conversionlog.txt
+		echo "- - Found HDR file $mkv" >> $outputpath/conversionlog.txt
 		filename=${file::-4}
 		$ffmpeg -c:v hevc_cuvid -init_hw_device opencl=ocl:0.0 -filter_hw_device ocl -i "$mkv" -threads 0 -map 0:0 -codec:v:0 libx264 -pix_fmt yuv420p -preset $preset -tune $tune -crf $crf -maxrate 20029988 -bufsize 40059976 -profile:v:0 high -level 41 -x264opts:0 subme=0:me_range=4:rc_lookahead=10:me=dia:no_chroma_me:8x8dct=0:partitions=none  -force_key_frames:0 "expr:gte(t,0+n_forced*3)" -vf "hwupload,tonemap_opencl=format=nv12:primaries=bt709:transfer=bt709:matrix=bt709:tonemap=hable:desat=0:threshold=0.8:peak=100,hwdownload,format=nv12"  -avoid_negative_ts disabled -max_muxing_queue_size 9999 -c:a copy -map 0:a -c:s copy -map 0:s -movflags -use_metadata_tags  -metadata title="$filename - HDR tonemaped by youtube.com/tontonjo" "$outputpath/$filename - HDR.mkv"
 		exitcode=$?
