9af53e26141baf9e4bf04f8a16176df49f4d3969
Tonton Jo <60965766+Tontonjo@users.noreply.github.com>
2022-04-24 15:02:47 +0200

    Add check for 10 bit content even if x264

--- compatibility_checker.sh
+++ compatibility_checker.sh
@@ -21,8 +21,9 @@
 
 # ------------- Settings -------------------------
 inputpath=/media/films
-unwantedsub="pgs"
-unwantedformat="HEVC|265"
+unwantedsub="pgs|test"
+unwanted265format="HEVC|265"
+unwanted264format="10"
 unwantedaudio="dts|ac3"
 unwantedcolormap="smpte2084|bt2020nc|bt2020"
 
@@ -42,14 +43,21 @@ echo "- Starting Check of .mkv in $inputpath" >> $inputpath/checklog.txt
 	for mkv in `find $inputpath | sort -h | grep .mkv`
 	do
 	ffprobeoutput=$($ffprobe -show_streams $mkv)
-	if echo "$ffprobeoutput" | grep codec | grep -Eqi "$unwantedformat" ; then
-		echo "$mkv - Unwanted format ($unwantedformat)" >> $inputpath/checklog.txt 
+	if echo "$ffprobeoutput" | grep codec | grep -Eqi "$unwanted265format" ; then
+		echo "$mkv - Unwanted format ($unwanted265format)" >> $inputpath/checklog.txt 
 		if echo "$ffprobeoutput" | grep -Eqi "$unwantedcolormap" ; then
 		echo "$mkv - Found HDR colors ($unwantedcolormap)" >> $inputpath/checklog.txt
+
 		fi
+	elif echo "$ffprobeoutput" | grep profile | grep -Eqi "$unwanted264format" ; then
+			echo "$mkv - Unwanted format ($unwanted264format)" >> $inputpath/checklog.txt 
 	fi
+	if  [[ $1 = "--ignore-sub" ]]; then
+	echo "- Ignoring sub check"
+	else
 	if echo "$ffprobeoutput" | grep codec | grep -Eqi "$unwantedsub" ; then
 	echo "$mkv - Unwanted subtitles format found ($unwantedsub)" >> $inputpath/checklog.txt
+	fi
 	fi
 	if echo "$ffprobeoutput" | grep codec | grep -Eqi "$unwantedaudio" ; then
 	echo "$mkv - Unwanted audio format found ($unwantedaudio)" >> $inputpath/checklog.txt
